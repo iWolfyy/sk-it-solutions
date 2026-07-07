3c076f1896fcdbb97137e69519c1d070a886fff2
Pavithra Wijesooriya <Pavithra.w@lnp.com>
2026-07-07 16:33:10 +0530

    vite fixed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   // Add the base property here:
-  base: "/sk-it-solutions/", 
+  base: "/", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
